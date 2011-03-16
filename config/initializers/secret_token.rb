8ec40a9624cbcf195f4040e0e3ecd911c23973fb
ThomasJoyce <thomasjoyce@gmail.com>
2011-03-15 19:39:50 -0700

    Basic install of a new project in rails 3.0.5

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JesusSpree::Application.config.secret_token = '7fa4b4c40cebbd941de1ed6a2a88e961a5f1b1a722662217229021598e85c138c745ab614b56019e3e24934f5073b20659b77bc2911c85a5ce9c8f1abd793b2e'
