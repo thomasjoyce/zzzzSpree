fd4cd74529c54b1493a94670ce2c61277a0fe636
ThomasJoyce <thomasjoyce@gmail.com>
2011-04-14 19:12:37 -0700

    Return results order by created_at desc order

--- vendor/extensions/antzoom_deal_content/app/models/deal.rb
+++ vendor/extensions/antzoom_deal_content/app/models/deal.rb
@@ -1,4 +1,5 @@
 class Deal < ActiveRecord::Base
+  default_scope :order => 'created_at desc'
   
   as_enum :category_type, [ :deal, :freebie ], :column => 'category_type'
   #validates_as_enum :category_type
@@ -7,6 +8,8 @@ class Deal < ActiveRecord::Base
   belongs_to :brand
   belongs_to :store
   #has_many :comments
+  
+  
 
 
 end
